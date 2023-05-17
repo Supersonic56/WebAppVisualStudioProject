63df824db26083d11423a8e2a068943039756c01
Supersonic56 <lucykiszczakphd@gmail.com>
2023-05-16 22:56:47 -0700

    Web App 1st version

---
+++ MyFavSong/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿let angle = 0,
+    img = document.getElementById('img_container');
+document.getElementById('button').onclick = function () {
+    angle = (angle + 90) % 360;
+    img.className = "rotate" + angle;
+}
+
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
